4195a0e67957ec46bcfccbbf9366c5e3085bbd27
akiyoko <akiyoko@users.noreply.github.com>
2019-03-30 07:52:52 +0900

    少し本格的な画面を作ろう

--- shop/urls.py
+++ shop/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'shop'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create/', views.create, name='create'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,19 +1,46 @@
-import logging
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 
+from .forms import BookForm
 from .models import Book
 
-logger = logging.getLogger(__name__)
 
-
-class IndexView(View):
+class BookIndexView(View):
     def get(self, request, *args, **kwargs):
         context = {
+            # 本レコードの一覧を取得
             'book_list': Book.objects.all(),
         }
+        # ショップ画面を表示
         return render(request, 'shop/book_list.html', context)
 
 
-index = IndexView.as_view()
+index = BookIndexView.as_view()
+
+
+class BookCreateView(View):
+    def get(self, request, *args, **kwargs):
+        context = {
+            # 空のフォームを作成
+            'form': BookForm(),
+        }
+        # 登録画面を表示
+        return render(request, 'shop/book_form.html', context)
+
+    def post(self, request, *args, **kwargs):
+        # リクエストの入力データをフォームオブジェクトに変換
+        form = BookForm(request.POST)
+        # バリデーション実行
+        if not form.is_valid():
+            context = {
+                'form': form,
+            }
+            # 登録画面を再表示
+            return render(request, 'shop/book_form.html', context)
+        # モデルオブジェクトを保存
+        form.save()
+        # ショップ画面にリダイレクト
+        return redirect('/shop/')
+
+
+create = BookCreateView.as_view()
